7a0fea28e75f3267c441dfa468f88121cc51c1a6
wrench554 <90932687+wrench554@users.noreply.github.com>
2022-09-25 09:46:36 +0600

    Update musico.config.js

--- musico.config.js
+++ musico.config.js
@@ -6,8 +6,8 @@
  * @note you can add other properties to this object by yourself and access them in your code using `client.config.<your-property>`
  */
 const config = {
-	botId: "",
-	botToken: "",
-	ownerId: "",
+	botId: "1023430878820569109",
+	botToken: "MTAyMzQzMDg3ODgyMDU2OTEwOQ.GvXd10.Ak0CkGdFVXNfuG4W77wVMZwjTn_Fx2qS_j9X-U",
+	ownerId: "803955450386972697",
 };
 module.exports = config;
